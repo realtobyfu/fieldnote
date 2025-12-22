f4574464c220a56fa1e0ff12b138edb798e9f971
Tobias Fu <fuz.sh0710@gmail.com>
2025-12-21 22:43:59 -1000

    Initial Commit

---
+++ Fieldnote/FieldnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieldnoteApp.swift
+//  Fieldnote
+//
+//  Created by Tobias on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FieldnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
